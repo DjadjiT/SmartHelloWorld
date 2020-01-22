93b0048041a3a6a1b1f536f216011ce670c360b6
djadjiT <djadji.traore75@gmail.com>
2020-01-22 10:07:03 +0100

    Test in comment

--- SmartHelloWorld/Tests/UnitTest1.cs
+++ SmartHelloWorld/Tests/UnitTest1.cs
@@ -7,8 +7,12 @@ namespace Tests
 {
     public class UnitTest1
     {
+        /*
         [Theory]
         [InLineData(7, "", "Good morning world")]
+        [InLineData(11, "", "Good morning world")]
+        [InLineData(11, "test", "Good morning test")]
+        [InLineData(9, "X", "Good morning X")]
         public void should_return_MorningGrettings_when_dayTime_is_morning(int hour, String name, String expected)
         {
             string sut = SmartHelloWorld.WriteAGreetings(name, new DateTimeImpl(hour));
@@ -17,6 +21,9 @@ namespace Tests
 
         [Theory]
         [InLineData(15, "", "Good afternoon world")]
+        [InLineData(17, "", "Good afternoon world")]
+        [InLineData(13, "test", "Good afternoon test")]
+        [InLineData(15, "X", "Good afternoon ")]
         public void should_return_AfternoonGrettings_when_dayTime_is_afternoon(int hour, String name, String expected)
         {
             string sut = SmartHelloWorld.WriteAGreetings(name, new DateTimeImpl(hour));
@@ -25,11 +32,13 @@ namespace Tests
 
         [Theory]
         [InLineData(23, "", "Good night world")]
+        [InLineData(5, "test", "Good night test")]
+        [InLineData(21, "", "Good night world")]
+        [InLineData(23, "X", "Good night X")]
         public void should_return_NightGrettings_when_dayTime_is_night(int hour, String name, String expected)
         {
             string sut = SmartHelloWorld.WriteAGreetings(name, new DateTimeImpl(hour));
             Check.That(sut).isEqualsTo(expected);
-        }
-
+        } */
     }
 }
